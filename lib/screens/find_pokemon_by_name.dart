a7e0dd3dd3c7f63e62f8713d7c050ecefb9f8e0a
Ícaro Motta <71195769+icarosuper@users.noreply.github.com>
2023-06-02 10:15:07 -0300

    Adicionando exibição de carregamento.

--- lib/screens/find_pokemon_by_name.dart
+++ lib/screens/find_pokemon_by_name.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:litedex/http/webclient.dart';
+import 'package:litedex/model/pokemon.dart';
 import 'package:litedex/screens/pokemon_info.dart';
 
 class FindPokemonByName extends StatefulWidget {
@@ -13,18 +14,31 @@ class _FindPokemonByNameState extends State<FindPokemonByName>
     with SingleTickerProviderStateMixin {
   final TextEditingController _nameController = TextEditingController();
   late AnimationController _controller;
+  bool loading = false;
 
-  searchPokemon() {
+  setLoading(bool isLoading) {
+    setState(() {
+      loading = isLoading;
+    });
+  }
+
+  goToInfoScreen(Pokemon? pokemon) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => PokemonInfoScreen(pokemon: pokemon),
+    ));
+  }
+
+  searchPokemon() async {
+    if (_nameController.text == '') return;
+
     final pokemonName = _nameController.text.toLowerCase().trim();
     _nameController.text = '';
+    setLoading(true);
 
-    getPokemonByName(pokemonName).then(
-      (pokemon) => Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => PokemonInfoScreen(pokemon: pokemon),
-        ),
-      ),
-    );
+    final pokemon = await getPokemonByName(pokemonName);
+
+    setLoading(false);
+    goToInfoScreen(pokemon);
   }
 
   @override
@@ -45,6 +59,14 @@ class _FindPokemonByNameState extends State<FindPokemonByName>
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
+          Padding(
+              padding: const EdgeInsetsDirectional.only(top: 2, bottom: 8),
+              child: Visibility(
+                  visible: loading,
+                  maintainSize: true,
+                  maintainAnimation: true,
+                  maintainState: true,
+                  child: const LinearProgressIndicator())),
           TextFormField(
             controller: _nameController,
             decoration: const InputDecoration(
